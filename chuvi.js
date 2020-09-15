ee159d416325091f766e5e3b5a4b4cf1798bc6cd
unknown <trantuyet.fightforfreedom@gmail.com>
2020-09-15 14:24:24 +0700

    abcbc

---
+++ chuvi.js
@@ -0,0 +1,7 @@
+function tinh() {
+    let r = document.getElementById("c").value;
+    const PI = 3.1415926535;
+    let c = 2* parseFloat(r) * PI;
+    document.write("Chu vi hình tròn là : " + c);
+
+}
